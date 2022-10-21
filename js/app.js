975d8eb49f853592226db0383eab83aa52a4551c
Anton Kulbitskiy <pa1nkller8879@gmail.com>
2022-10-21 19:41:33 +0300

    prettier fix

--- js/app.js
+++ js/app.js
@@ -1,40 +1,43 @@
 const API_KEY = "197446f3-9eab-475d-a19f-9ffdb741466c";
-const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
-const API_URL_SEARCH = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
-const API_URL_MOVIE_DETAILS = "https://kinopoiskapiunofficial.tech/api/v2.2/films/"
+const API_URL_POPULAR =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1";
+const API_URL_SEARCH =
+  "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
+const API_URL_MOVIE_DETAILS =
+  "https://kinopoiskapiunofficial.tech/api/v2.2/films/";
 
 getMovies(API_URL_POPULAR);
 
 async function getMovies(url) {
-    const resp = await fetch(url, {
-        headers: {
-            "Content-Type": "application/json",
-            "X-API-KEY": API_KEY,
-        },
-    });
-    const respData = await resp.json();
-    showMovies(respData);
+  const resp = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const respData = await resp.json();
+  showMovies(respData);
 }
 
 function getClassByRate(vote) {
-    if (vote >= 7) {
-        return "green";
-    } else if (vote > 5) {
-        return "orange";
-    } else {
-        return "red";
-    }
+  if (vote >= 7) {
+    return "green";
+  } else if (vote > 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
 }
 
 function showMovies(data) {
-    const moviesEl = document.querySelector(".movies");
-    // Очищаем предыдущие фильмы
-    document.querySelector(".movies").innerHTML = "";
+  const moviesEl = document.querySelector(".movies");
+  // Очищаем предыдущие фильмы
+  document.querySelector(".movies").innerHTML = "";
 
-    data.films.forEach((movie) => {
-        const movieEl = document.createElement("div");
-        movieEl.classList.add("movie");
-        movieEl.innerHTML = `
+  data.films.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+    movieEl.innerHTML = `
         <div class="movie__cover-inner">
         <img
           src="${movie.posterUrlPreview}"
@@ -46,53 +49,53 @@ function showMovies(data) {
       <div class="movie__info">
         <div class="movie__title">${movie.nameRu}</div>
         <div class="movie__category">${movie.genres.map(
-            (genre) => ` ${genre.genre}`
+          (genre) => ` ${genre.genre}`
         )}</div>
         ${
-            movie.rating &&
-            `
+          movie.rating &&
+          `
         <div class="movie__average movie__average--${getClassByRate(
-                movie.rating
-            )}">${movie.rating}</div>
+          movie.rating
+        )}">${movie.rating}</div>
         `
         }
       </div>
         `;
-        movieEl.addEventListener("click", () => openModal(movie.filmId))
-        moviesEl.appendChild(movieEl);
-    });
+    movieEl.addEventListener("click", () => openModal(movie.filmId));
+    moviesEl.appendChild(movieEl);
+  });
 }
 
 const form = document.querySelector("form");
 const search = document.querySelector(".header__search");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
-    if (search.value) {
-        getMovies(apiSearchUrl);
+  const apiSearchUrl = `${API_URL_SEARCH}${search.value}`;
+  if (search.value) {
+    getMovies(apiSearchUrl);
 
-        search.value = "";
-    }
+    search.value = "";
+  }
 });
 
 // Модальное окно
 const modalEl = document.querySelector(".modal");
 
 async function openModal(id) {
-    const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
-        headers: {
-            "Content-Type": "application/json",
-            "X-API-KEY": API_KEY,
-        },
-    });
-    const respData = await resp.json();
+  const resp = await fetch(API_URL_MOVIE_DETAILS + id, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const respData = await resp.json();
 
-    modalEl.classList.add("modal--show");
-    document.body.classList.add("stop-scrolling");
+  modalEl.classList.add("modal--show");
+  document.body.classList.add("stop-scrolling");
 
-    modalEl.innerHTML = `
+  modalEl.innerHTML = `
     <div class="modal__card">
      <div class="box-1">
         <img class="modal__movie-backdrop" src="${respData.posterUrl}" alt="">
@@ -102,31 +105,41 @@ async function openModal(id) {
         <span class="modal__movie-release-year"> - ${respData.year}</span>
       <ul class="modal__movie-info">
         <div class="loader"></div>
-        <li class="modal__movie-genre">Жанр - ${respData.genres.map((el) => `<span>${el.genre}</span>`)}</li>
-        ${respData.filmLength ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>` : ''}
-        <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${respData.webUrl}</a></li>
-        <li class="modal__movie-overview">Описание - ${respData.description}</li>
+        <li class="modal__movie-genre">Жанр - ${respData.genres.map(
+          (el) => `<span>${el.genre}</span>`
+        )}</li>
+        ${
+          respData.filmLength
+            ? `<li class="modal__movie-runtime">Время - ${respData.filmLength} минут</li>`
+            : ""
+        }
+        <li >Сайт: <a class="modal__movie-site" href="${respData.webUrl}">${
+    respData.webUrl
+  }</a></li>
+        <li class="modal__movie-overview">Описание - ${
+          respData.description
+        }</li>
       </ul>
     </div>
     </div>
-  `
+  `;
 }
 
 function closeModal() {
-    modalEl.classList.remove("modal--show");
-    document.body.classList.remove("stop-scrolling");
+  modalEl.classList.remove("modal--show");
+  document.body.classList.remove("stop-scrolling");
 }
 
 // Закрытие по клику вне модального окна
 window.addEventListener("click", (e) => {
-    if (e.target === modalEl) {
-        closeModal();
-    }
-})
+  if (e.target === modalEl) {
+    closeModal();
+  }
+});
 
 // Закрытие по нажатию ESC на клавиатуре
 window.addEventListener("keydown", (e) => {
-    if (e.keyCode === 27) {
-        closeModal();
-    }
-})
+  if (e.keyCode === 27) {
+    closeModal();
+  }
+});
